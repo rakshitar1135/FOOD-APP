c7c3a5ac204264ac3603a48cd2e7a63f7351b4c7
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-11-25 12:55:11 +0530

    Adding  A form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,5 @@
 import classes from './MealItem.module.css';
-
+import MealItemForm from './MealItemForm'
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
 
@@ -11,6 +11,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div></div>
+      <MealItemForm/>
     </li>
   );
 };
