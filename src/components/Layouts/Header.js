4562d096ca683ccd8636cab31b60e8b17e8fadcb
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-11-26 11:12:31 +0530

    MANAGING CART AND MODAL STATE

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { Fragment } from "react";
-import FOODNEW from '../../assets/FOODNEW.jpeg'
-import classes from './Header.module.css'
-import HeaderCartButton from "./HeaderCartButton";
-const Header=(props)=>{
-    return (<Fragment>
-        <header className={classes.header}>
-        <h1>SOUTH INDIAN MEALS</h1>
-        <HeaderCartButton></HeaderCartButton>
-        </header>
-        <div className={classes['main-image']}>
-        <img src={FOODNEW} alt='A table full of delicious Food'></img>
-        </div>
-    </Fragment>)
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import FOODNEW from '../../assets/FOODNEW.jpeg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+      <img src={FOODNEW} alt='A table full of delicious Food'></img>
+      </div>
+    </Fragment>
+  );
 };
+
 export default Header;
